59af89bdbf7427a7006fafa9b6502bae6cae6a19
miraleJ <miralek@gmail.com>
2021-05-06 09:28:27 +0300

    send body from front end to beck

--- client/src/components/AddCourse.component.jsx
+++ client/src/components/AddCourse.component.jsx
@@ -5,24 +5,21 @@ export default function AddCourse() {
     const [courseName, setCourseName] = useState('');
 
     const nameHandler = (e) => {
-        console.log('goooooooooo');
-        setCourseName('t13')
-        // setCourseName(this.target)
+        setCourseName(e.target.value)
     }
 
     const buttonHandler = async () => {
-        console.log('yes!');
         await axios.post('https://first-aid-courses.herokuapp.com/api/courses/',{
-                "courseName" : `${courseName}`,
-                "length" : 1.5,
-                "price" : 159.9
+            "courseName" : courseName,
+            "length" : 1.5,
+            "price" : 159.9
         });
     }    
 
     return (
         <div className="add-course-section">
-            <label htmlFor="Course name" className="input-lable" onChange={nameHandler}>New course name: </label>
-            <input type="text" className="input-area" id="new-course-name"/>
+            <label htmlFor="Course name" className="input-lable" >New course name: </label>
+            <input type="text" className="input-area" id="new-course-name" onChange={nameHandler}/>
             <label htmlFor="Course length" className="input-lable">New course length: </label>
             <input type="text" className="input-area"/>
             <label htmlFor="Course price" className="input-lable">New course price: </label>
