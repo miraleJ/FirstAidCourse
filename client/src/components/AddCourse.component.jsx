5c0cb3a02c09002defb81b4263d74721826c4747
miraleJ <miralek@gmail.com>
2021-05-06 09:52:17 +0300

    full add course from front

--- client/src/components/AddCourse.component.jsx
+++ client/src/components/AddCourse.component.jsx
@@ -3,16 +3,26 @@ import axios from 'axios'
 
 export default function AddCourse() {
     const [courseName, setCourseName] = useState('');
+    const [courseLength, setCourseLength] = useState('');
+    const [coursePrice, setCoursePrice] = useState('');
 
     const nameHandler = (e) => {
         setCourseName(e.target.value)
     }
 
+    const lengthHandler = (e) => {
+        setCourseLength(e.target.value)
+    }
+
+    const priceHandler = (e) => {
+        setCoursePrice(e.target.value)
+    }
+
     const buttonHandler = async () => {
         await axios.post('https://first-aid-courses.herokuapp.com/api/courses/',{
             "courseName" : courseName,
-            "length" : 1.5,
-            "price" : 159.9
+            "length" : courseLength,
+            "price" : coursePrice
         });
     }    
 
@@ -21,9 +31,9 @@ export default function AddCourse() {
             <label htmlFor="Course name" className="input-lable" >New course name: </label>
             <input type="text" className="input-area" id="new-course-name" onChange={nameHandler}/>
             <label htmlFor="Course length" className="input-lable">New course length: </label>
-            <input type="text" className="input-area"/>
+            <input type="text" className="input-area" id="new-course-length" onChange={lengthHandler}/>
             <label htmlFor="Course price" className="input-lable">New course price: </label>
-            <input type="text" className="input-area"/>
+            <input type="text" className="input-area" id="new-course-price" onChange={priceHandler}/>
             <button onClick={buttonHandler}>Add New Course</button>
         </div>
     )
