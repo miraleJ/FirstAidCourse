5b9badad919fb72edfabc344374af0fc4ecf70a5
miraleJ <miralek@gmail.com>
2021-05-13 15:44:33 +0300

    בדד

--- client/src/components/HighlightIn2Circles/HighlightIn2Circles.component.jsx
+++ client/src/components/HighlightIn2Circles/HighlightIn2Circles.component.jsx
@@ -6,15 +6,13 @@ export default function HighlightIn2Circles({ title, text, picture }) {
     return (
         <div className='highlight-in-2-circles' >
             {/* back circle */}
-            <div className="back-circle" >
-                aaa
-             {/* style={
-                // {background: `url(${picture})`}}> */}
+            <div className="back-circle" style={{background: `url("https://images.unsplash.com/photo-1611764461465-09162da6465a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2000&q=80") no-repeat center`,
+            backgroundSize: '100%'}}>
             </div>
             {/* front circle */}
             <div className="front-circle">
                 <h3 className="highlight-title">TIP:</h3>
-                <p className="highlight-txt">Be safe! Be safe!</p>
+                <p className="highlight-txt">Be safe!</p>
             </div>
         </div>
     )
